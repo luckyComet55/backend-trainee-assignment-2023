7b01af9a85b655b36ff3969001303ddac43795e5
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-08-25 15:14:32 +0300

    Added basic handler

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := flag.String("port", "3003", "port the server will listen to")
+	flag.Parse()
+
+	r := chi.NewRouter()
+	r.Get("/", helloRootHandler)
+	log.Fatal(http.ListenAndServe(":"+*port, r))
+}
